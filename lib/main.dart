ed8667926c1254b6a07c3f23fbbcd6bf8ad250dd
akshya1205 <119163145+akshya1205@users.noreply.github.com>
2024-03-08 19:22:13 +0530

    updated app

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,20 @@
 import 'package:flutter/material.dart';
+import 'Calculator_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Button-like Text Field'),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Center(
-            child: MyButton(),
-          ),
-        ),
-      ),
+        title: 'Calculator',
+        theme: ThemeData.dark(),
+        home: const Calculatorscreen()
     );
-  }
-}
-
-class MyButton extends StatefulWidget {
-  @override
-  _MyButtonState createState() => _MyButtonState();
-}
-
-class _MyButtonState extends State<MyButton> {
-  String _value = '';
-
-  void _onTap() {
-    setState(() {
-      _value = 'Button tapped!';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController _controller1 = TextEditingController();
-    TextEditingController _controller2 = TextEditingController();
-    return
-    Row(
-      children: [
-        Expanded(
-          child: TextField(
-            controller: _controller1,
-            decoration: InputDecoration(
-              labelText: 'Text Field 1',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-            onChanged: (value) {
-              setState(() {
-                
-              });
-            },
-          ),
-        ),
-        SizedBox(width: 16),
-        Expanded(
-          child: TextField(
-            controller: _controller2,
-            decoration: InputDecoration(
-              labelText: 'Text Field 2',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-            onChanged: (value) {
-              // Do something when the text field changes
-            },
-          ),
-        ),
-      ],
-    )
   }
 }
